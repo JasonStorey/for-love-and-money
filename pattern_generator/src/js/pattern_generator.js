ffcac9c82169d2903209d7bc620ca8fdb4aec5f1
Jason Storey <jason.andrew.storey@gmail.com>
2014-03-07 02:00:12 +0000

    Added basic graphing interactions

--- pattern_generator/src/js/pattern_generator.js
+++ pattern_generator/src/js/pattern_generator.js
@@ -1,3 +1,61 @@
-$(document).ready(function(){
-	console.log('Hello, world');
+var PatternGenerator = function(selector) {
+
+	var container = $(selector),
+		container_width = container.width(),
+		container_height = container.height(),
+		resolution = 300,
+		sections = [];
+
+	function init() {
+		var i,
+			section_container,
+			section,
+			section_width;
+
+		for(i = 0; i < resolution; i++) {
+			section_width = container_width / resolution;
+
+			section_container = $('<div>');
+			section = $('<div>');
+
+			section_container.addClass('section_container');
+			section.addClass('section');
+
+			section_container.width(section_width);
+			section_container.css('left', (section_width * i)+ 'px');
+
+			section_container.append(section);
+			sections.push(section_container);
+		}
+	}
+
+	function render() {
+		container.append(sections);
+	}
+
+	function listenForHover() {
+		container.mousemove(function(e){			
+			if(e.target.className !== 'section_container') {
+				return;
+			}
+			moveSection(e.target.children[0], container_height - e.clientY);
+		});
+	}
+
+	function moveSection(sectionElem, mouseY) {
+		$(sectionElem).height(((mouseY / container_height) * 100) + '%');
+	}
+
+	return {
+		init: function() {
+			init();
+			render();
+			listenForHover();
+		}
+	};
+};
+
+$(document).ready(function() {
+	var pattern1 = PatternGenerator('.pattern');
+	pattern1.init();
 });
